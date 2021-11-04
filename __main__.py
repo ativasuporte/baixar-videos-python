73de4e3ac5880e32b1eec824bcf397036746a283
Ativa Suporte <suporteativanautica@gmail.com>
2021-11-04 17:39:17 -0300

    Inicial

---
+++ __main__.py
@@ -0,0 +1,21 @@
+#João Pedro
+import os
+import sys
+
+home = os.path.expanduser("~")
+default_path = home + "\\Downloads\\youtube-dl"
+
+
+def main():
+
+    print("Verificando a instalação do youtube-dl...")
+
+    os.system("pip install --upgrade youtube-dl")
+
+    url = input("Url do vídeo: ")
+
+    os.system("youtube-dl " + url)
+
+    print("Vídeo baixado em: " + default_path)
+
+main()

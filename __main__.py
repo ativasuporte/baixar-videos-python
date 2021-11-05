bd206d56289bfe7360921630d2be6ccb79c336d6
Ativa Suporte <suporteativanautica@gmail.com>
2021-11-05 15:04:42 -0300

    Build alpha v1.0.0
    Co-Authored-By: jpricardo <esse.email.memo@gmail.com>

--- __main__.py
+++ __main__.py
@@ -2,35 +2,42 @@
 import os
 import sys
 import time
+import pip
+import subprocess
+from subprocess import call
 
 home = os.path.expanduser("~")
 default_path = home + "\\Downloads\\youtube-dl"
 
 
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+
 def main():
     #cabecinha
     os.system("cls")
-
     print("Verificando a instalação do youtube-dl...")
 
-    os.system("pip install --upgrade youtube-dl")
-
-    os.system("cls")
+    try:
+        install("youtube-dl")
+    except:
+        print("Houve um problema na instalação...")
 
     url = input("Url do vídeo: ")
-
     comando = "youtube-dl -o " + default_path + "\\%(title)s.%(ext)s " + url
-
-    os.system(comando)
+    try:
+        call(comando)
+    except:
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 
     print("Vídeo baixado em: " + default_path)
 
     while True:
-
         escolha = input("Deseja exibir a pasta de downloads? (y/n)")
-
         if escolha.lower() == "y":
-            comando = "start explorer.exe " + default_path
+            comando = "start explorer " + default_path
             os.system(comando)
             break
         if escolha.lower() == "n":
@@ -41,8 +48,8 @@ def main():
 
     time.sleep(2)
     os.system("cls")
-    print("Obrigado por utilizar meu script!")
+    print("Valeu!")
+    time.sleep(5)
     return
-
 
 main()
